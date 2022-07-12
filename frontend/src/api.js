b18bd2a0fd4db3ab12fefa84eeab55e5c40af31e
Geoffrey Israel <israelgeoffrey13@gmail.com>
2022-07-12 14:26:33 +0100

    first commit

---
+++ frontend/src/api.js
@@ -0,0 +1,13 @@
+/* 
+const API_URL = () => {
+
+// let API_BASE_URL = process.env.REACT_APP_LOC_API_BASE;
+// if (process.env.NODE_ENV === 'production') {
+//   API_BASE_URL = process.env.REACT_APP_PROD_API_BASE;
+// }
+let API_BASE_URL = process.env.REACT_APP_LOC_API_BASE;
+return API_BASE_URL
+}
+export default API_URL */
+
+export const apiEndpoint = process.env.REACT_APP_LOC_API_BASE
